8b2dc8416ab62a593da1caa4f5f23f54c46d1ff3
Alexandr Velikiy <39257464+VargBurz@users.noreply.github.com>
2018-10-12 15:36:29 +0300

    add  vertical wall and fireblast
    * добавлены несколько "пустых" классов - марио, фаербол
    * добавлена одна вертикальная стена
    * добавлен фаербол
    * в стенах тоже есть монетки
    * фаербол может разрушать стену

--- mario.js
+++ mario.js
@@ -10,7 +10,11 @@ var LIFE = 3
 
 var DIR = 'stay'
 
+var DIR_X = 'right'
+
 var UP_LEVEL = 0
+
+var FB_LEVEL = 0
 
 var JUMP_LENGTH = 8
 
@@ -59,6 +63,14 @@ var TURTLE_PLACE = [
 		},
 ]
 
+var FIRE_BALL = [
+		{
+			x:30,
+			y:30,
+		},
+]
+
+
 var WALL_LENGTH = 6
 
 function drawMario() {
@@ -100,6 +112,13 @@ function drawCoins() {
 	}
 }
 
+function drawFireBall() {
+		var x = FIRE_BALL[0].x*15
+		var y = FIRE_BALL[0].y*15
+		ctx.fillStyle = 'red'
+		ctx.fillRect(x + 5, y + 5, 5, 5)
+}
+
 function createLand() {
 	for (var i = 1; i < 100; i++){
 		LAND[i] = {x: i, y: 31}
@@ -117,6 +136,14 @@ function createWall() {
 		WALL_PLACE[i] = {x: WALL_PLACE[i - 1].x + 1, y: WALL_PLACE[WALL_LENGTH].y, l: 2}
 	}
 	
+	var vertical_wall = [{x: 38, y: 30, l: 2}]
+	
+	for (var i = 1; i < 10; i++) {
+		vertical_wall[i] = {x: vertical_wall[0].x, y: vertical_wall[i - 1].y - 1, l: 2}
+	}
+	
+	WALL_PLACE = WALL_PLACE.concat(vertical_wall)	
+	
 }
 
 function createCoins() {
@@ -131,6 +158,13 @@ function createCoins() {
 		var r = Math.round(Math.random())
 		if (r == 1) {
 			COINS.push({x:WALL_PLACE[i].x, y:WALL_PLACE[i].y - 1})
+		}
+	}
+	
+	for (var i = 0; i < WALL_PLACE.length; i++) {
+		var r = Math.round(Math.random())
+		if (r == 1) {
+			COINS.push({x:WALL_PLACE[i].x, y:WALL_PLACE[i].y})
 		}
 	}
 }
@@ -163,10 +197,48 @@ function down() {
 	}
 }
 
+function checkFB() {
+	if (FB_LEVEL > 0) {
+		if (DIR_X == 'right') {
+			var o = {}
+			o.x = FIRE_BALL[0].x + 1
+			o.y = FIRE_BALL[0].y
+			if(isExist(WALL_PLACE, o) == false) {
+				FIRE_BALL[0] = {x:FIRE_BALL[0].x + 1, y:FIRE_BALL[0].y}
+				drawFireBall()
+				FB_LEVEL = FB_LEVEL - 1 
+			}
+			else {
+				FIRE_BALL[0] = {x:FIRE_BALL[0].x + 1, y:FIRE_BALL[0].y}
+				hitWall(o)
+				FB_LEVEL = 0
+			}
+		}
+		else if(DIR_X == 'left') {
+			FIRE_BALL[0] = {x:FIRE_BALL[0].x - 1, y:FIRE_BALL[0].y}
+			drawFireBall()
+			FB_LEVEL = FB_LEVEL - 1
+		}
+	}
+}
+
+function throwFireBall() {
+	if(DIR_X == 'right') {
+		FB_LEVEL = 8
+		FIRE_BALL[0] = {x:MARIO_PLACE[0].x + 1, y: MARIO_PLACE[0].y}
+		drawFireBall()
+	}
+	else {
+		FB_LEVEL = 8
+		FIRE_BALL[0] = {x:MARIO_PLACE[0].x - 1, y: MARIO_PLACE[0].y}
+		drawFireBall()
+	}
+}
 
 document.onkeydown = function checkKey(event) {
 	if(accessKeyboard) {
 		if(event.keyCode == 37) {
+			DIR_X = 'left'
 			var o = {}
 			o.x = MARIO_PLACE[0].x - 1
 			o.y = MARIO_PLACE[0].y
@@ -175,6 +247,7 @@ document.onkeydown = function checkKey(event) {
 			}
 		}
 		if(event.keyCode == 39) {
+			DIR_X = 'right'
 			var o = {}
 			o.x = MARIO_PLACE[0].x + 1
 			o.y = MARIO_PLACE[0].y
@@ -192,6 +265,12 @@ document.onkeydown = function checkKey(event) {
 				//}
 			}
 		}
+		if(event.keyCode == 101) {
+			if (FB_LEVEL == 0){
+				throwFireBall()
+			}
+		}
+		
 	}
 	accessKeyboard = false
 }
@@ -221,11 +300,12 @@ function game() {
 	gravitation()	
 	document.onkeydown
 	checkJump()
-	turt_1.moveTurtle()
 	catchCoin()
+	drawCoins()
+	checkFB()
+	turt_1.moveTurtle()
 	drawWall()
 	drawLand()
-	drawCoins()
 	turt_1.drawTurtle()
 	drawMario()
 	drawScore()
@@ -290,14 +370,11 @@ function hitWall(o) {
 }
 
 class Turtle {
-	init() {
-		turt_dir = 'right'
-		
-	}
 	
 	constructor(number) {
 		this.number = number
 		this.turt_dir = 'right'
+		//this.place
 	}
 	
 	createTurtle(){
@@ -320,7 +397,7 @@ class Turtle {
 	
 	moveRight() {
 		this.turt_dir = 'right'
-		if (TURTLE_PLACE[0].x < 45 + 4) {
+		if (TURTLE_PLACE[0].x < this.place + 4) {
 			TURTLE_PLACE[0].x = TURTLE_PLACE[0].x + 0.5
 		}
 		else {
@@ -330,7 +407,7 @@ class Turtle {
 	
 	moveLeft() {
 		this.turt_dir = 'left'
-		if (TURTLE_PLACE[0].x > 45 - 4) {
+		if (TURTLE_PLACE[0].x > this.place - 4) {
 			TURTLE_PLACE[0].x = TURTLE_PLACE[0].x - 0.5
 		}
 		else {
@@ -354,9 +431,58 @@ class Turtle {
 	}
 	
 }
+
+class Mario {
+	
+	constructor(x, y) {
+		this.x = x
+		this.y = y
+	}
+	
+	createMario() {
+		MARIO_PLACE[0] = {x: this.x, y: this.y}
+	}
+	
+	drawMario() {
+		var x = MARIO_PLACE[0].x*15
+		var y = MARIO_PLACE[0].y*15
+		ctx.fillStyle = 'blue'
+		ctx.fillRect(x, y, 14, 14)
+	}
+	
+}
+
+class Fireball {
+	
+	constructor(x, y) {
+		this.x = x
+		this.y = y
+		this.FB_LEVEL = 8
+	}
+	
+	throwFireBall() {
+		if ((DIR_X == 'right') && (FIRE_BALL[0].x < this.x + 6)) {
+			FIRE_BALL[0] = {x: this.x + 1, y: this.y}
+			this.FB_LEVEL = this.FB_LEVEL - 1
+		}
+		else if ((DIR_X == 'left') && (FIRE_BALL[0].x > this.x - 6)) {
+			FIRE_BALL[0] = {x: this.x - 1, y: this.y}
+			this.FB_LEVEL = this.FB_LEVEL - 1
+		}
+	}
+	
+	drawFireBall() {
+		var x = FIRE_BALL[0].x*15
+		var y = FIRE_BALL[0].y*15
+		ctx.fillStyle = 'red'
+		ctx.fillRect(x + 5, y + 5, 5, 5)
+	}
+	
+}
 	
 createLand()
 createWall()
+console.log(WALL_PLACE)
 createCoins()
 var turt_1 = new Turtle(1)
 turt_1.createTurtle()
